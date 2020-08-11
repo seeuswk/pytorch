4557afa6c6cc4bdf5bd8e8be21cf95637d6029e9
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-10 22:59:03 -0700

    Added binary ops
    ghstack-source-id: d37f249fe89c30a71ca228b53ba862ead9e4ef03
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42536

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -25,6 +25,72 @@ std::vector<Tensor> foreach_add_scalar_kernel_fallback_(TensorList tensors, Scal
   return tensors.vec();
 }
 
+std::vector<Tensor> foreach_sub_scalar_kernel_fallback(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
+
+  std::vector<Tensor> result;
+  for (const auto& t: tensors) {
+    auto temp = t.sub(scalar);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_sub_scalar_kernel_fallback_(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
+
+  for (auto& t: tensors) {
+    t.sub_(scalar);
+  }
+
+  return tensors.vec();
+}
+
+std::vector<Tensor> foreach_div_scalar_kernel_fallback(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
+
+  std::vector<Tensor> result;
+  for (const auto& t: tensors) {
+    auto temp = t.div(scalar);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_div_scalar_kernel_fallback_(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
+
+  for (auto& t: tensors) {
+    t.div_(scalar);
+  }
+
+  return tensors.vec();
+}
+
+std::vector<Tensor> foreach_mul_scalar_kernel_fallback(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
+
+  std::vector<Tensor> result;
+  for (const auto& t: tensors) {
+    auto temp = t.mul(scalar);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_mul_scalar_kernel_fallback_(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
+
+  for (auto& t: tensors) {
+    t.mul_(scalar);
+  }
+
+  return tensors.vec();
+}
+
 std::vector<Tensor> foreach_add_list_kernel_fallback(TensorList tensors1, TensorList tensors2) {
   verify_list(tensors1, tensors2);
 
@@ -42,6 +108,72 @@ std::vector<Tensor> foreach_add_list_kernel_fallback_(TensorList tensors1, Tenso
 
   for (int i = 0; i < tensors1.size(); i++) {
     tensors1[i].add_(tensors2[i]);
+  }
+
+  return tensors1.vec();
+}
+
+std::vector<Tensor> foreach_sub_list_kernel_fallback(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors1.size(); i++) {
+    auto temp = tensors1[i].sub(tensors2[i]);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_sub_list_kernel_fallback_(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    tensors1[i].sub_(tensors2[i]);
+  }
+
+  return tensors1.vec();
+}
+
+std::vector<Tensor> foreach_mul_list_kernel_fallback(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors1.size(); i++) {
+    auto temp = tensors1[i].mul(tensors2[i]);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_mul_list_kernel_fallback_(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    tensors1[i].mul_(tensors2[i]);
+  }
+
+  return tensors1.vec();
+}
+
+std::vector<Tensor> foreach_div_list_kernel_fallback(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors1.size(); i++) {
+    auto temp = tensors1[i].div(tensors2[i]);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_div_list_kernel_fallback_(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    tensors1[i].div_(tensors2[i]);
   }
 
   return tensors1.vec();
